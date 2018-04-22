825a4997386ca2d30c7c06a4ef37431432d0ef91
roylevy1990 <hotmailsux62@gmail.com>
2018-04-22 15:03:17 +0300

    Update books.ts

--- src/models/books.ts
+++ src/models/books.ts
@@ -1,34 +1,25 @@
 export class Book {
-    public title: string;
-    public author: string;
-    public date: string;
-    public img: string;
-    public price: string;
+    public title: string = '';
+    public author: string = '';
+    public date: string = '';
+    public img: string = '';
+    public price: string = ((Math.random()*99)+1).toString();
+
     constructor(title?: string, author?: string, date?: string, img?: string, price?: string ){
         if(title){
             this.title = title;
-        }else{
-            this.title = ""
         }
         if(author){
             this.author = author;
-        }else{
-            this.author = ""
         }
         if(date){
             this.date = date;
-        }else{
-            this.date = ""
         }
         if(img){
             this.img = img;
-        }else{
-            this.img = ""
         }
         if(price){
             this.price = price;
-        }else{
-            this.price = ((Math.random()*99)+1).toString();
         }
     }
 }
